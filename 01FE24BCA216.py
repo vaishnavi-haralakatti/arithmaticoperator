ee38762ec12a53d9c5a48397f7c3dc52f77961ea
vaishnavi.haralakatti <01fe24bca216@kletech.ac.in>
2025-10-16 20:37:47 +0530

    arithmatic operation

---
+++ 01FE24BCA216.py
@@ -0,0 +1,19 @@
+# Program to perform addition and subtraction of two numbers
+
+# Input from user
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Perform operations
+addition = num1 + num2
+subtraction = num1 - num2
+mltiplicationu = num1*num2
+division = num1 / num2
+
+# Display results
+print("\nResults:")
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("mltiplicationu:", mltiplicationu)
+print("division:",division)
+
